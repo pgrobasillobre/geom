7c437468adda4db543c7fd7acf45841d99715915
Eva C SM <pgrobasillobre@gmail.com>
2024-07-02 18:01:21 +0200

    rotate 1 added

--- functions/rotate.py
+++ functions/rotate.py
@@ -28,6 +28,9 @@ def rotate_angles(inp):
    mol_rot = molecule.molecule()
 
    mol.read_geom(inp.geom_file,inp.move_geom_to_000)
+
+   # Adjust angles depending on direction
+   if inp.dir_axis_input[0] == '-': inp.angles = [360 - angle for angle in inp.angles]
  
    # Rotate over all angles
    for angle in inp.angles:
@@ -44,4 +47,45 @@ def rotate_angles(inp):
       output.logfile_close(out_log)
  
 
+#       ____        __        __          ___
+#      / __ \____  / /_____ _/ /____     <  /
+#     / /_/ / __ \/ __/ __ `/ __/ _ \    / / 
+#    / _, _/ /_/ / /_/ /_/ / /_/  __/   / /  
+#   /_/ |_|\____/\__/\__,_/\__/\___/   /_/   
+#                                          
 
+def rotate_1(inp):
+   #
+   """ 
+   Rotate molecule to a given angle.
+
+   :inp: input class
+   """
+   #
+   # Check input, create results folder, initialize logfile
+   inp.check_input_case()   
+   general.create_results_geom()
+   out_log = output.logfile_init()
+
+   # Initialize molecules class and read geometry
+   mol = molecule.molecule()
+   mol_rot = molecule.molecule()
+
+   mol.read_geom(inp.geom_file,inp.move_geom_to_000)
+
+   # Adjust angles depending on direction
+   if inp.dir_axis_input[0] == '-': inp.angle = 360.0 - inp.angle
+ 
+   # Rotate 
+   mol_rot = tools.rotate(mol,inp.angle,inp.dir_axis_input,mol_rot)
+
+   # Save rotate geometry
+   if inp.dir_axis_input[0] == '-': rot_file = f"{inp.geom_file[:-4]}_{inp.dir_axis_input}_degree_{abs(inp.angle - 360)}"
+   if inp.dir_axis_input[0] == '+': rot_file = f"{inp.geom_file[:-4]}_{inp.dir_axis_input}_degree_{inp.angle}" 
+       
+   output.print_geom(mol_rot, rot_file)
+
+   # Close and save logfile
+   output.logfile_close(out_log)
+ 
+      
