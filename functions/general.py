11e067b5770ad5e47fd47f724ea129aa917b6ed6
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-02-18 14:58:58 +0100

    redone: read command line

--- functions/general.py
+++ functions/general.py
@@ -4,109 +4,137 @@ import os
 from functions import output, create_geom
 
 # -------------------------------------------------------------------------------------
-def read_command_line(argv,inp):
-   #
-   """ 
-   Read command line
+def read_command_line(argv, inp):
+    if len(argv) < 2:
+        output.error("Missing command line arguments.")
+    command = argv[1]
+    if command in ['-h', '-help']:
+        print_help()
+    elif command == '-t' or command =='-t1':
+        parse_translation(argv, inp)
+    elif command == '-r' or command =='-r1':
+        parse_rotation(argv, inp)
+    elif command == '-min':
+        parse_min(argv,inp)
+    elif command == '-c':
+        parse_center(argv,inp)
+    elif command == '-mirror':
+        parse_mirror(argv, inp)
+    elif command == '-create':
+        parse_create(argv, inp)
+    elif command == '-merge':
+        parse_merge(argv,inp)
+    else:
+        output.error("ERROR: Option not recognized. Try python3 geom -h")
+# -------------------------------------------------------------------------------------
+def print_help():
+    """ 
+    Prints help message and exits 
+    """
+    #
+
+    help_text = '''
+
+      ==========
+      Execution:
+      ==========
+
+         * The inputs must contain each angle(degrees)/distance(Å) in different lines, without blank lines
+
+         ------------
+         Translation:
+         ------------
+
+         Distances in input (controlled dist. between 2 files):
+
+           -t distances_input geom1.xyz origin_CM_1{origin_CM_1_yes/no} geom2.xyz origin_CM_2{origin_CM_2_yes/no} axis{+-}{x/y/z} verbose{verbose_yes/no}
+
+         One translation:
+
+           -t1 shift geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}
+
+
+         ---------
+         Rotation:
+         ---------
+
+         Angles in input:
+
+           -r angles_input geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}
+
+         One rotation:
+
+           -r1 angle geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}
+
+
+         -----------------
+         Minimum Distance:
+         -----------------
+
+         -min geom1.xyz geom2.xyz
+
+
+         -------------------
+         Geometrical Center:
+         -------------------
+
+         -c geom.xyz
+
+
+         ------------------
+         Specular geometry:
+         ------------------
+
+         -mirror geom.xyz
+
+
+         -----------------
+         Generate Geometry
+         -----------------
+
+         Graphene:
+
+           Ribbon (X: zigzag | Y: armchair): -create -graphene rib X_length Y_length
+
+           Disk: -create -graphene disk radius
+
+           Ring: -create -graphene ring radius_out radius_in
+
+           Triangle: -create -graphene triangle edge_type{armchair/zigzag} side_length
+
+         Nanoparticles (Ag/Au/Na):
+
+           Sphere: -create -sphere atom_type radius center_x center_y center_z [optional: -alloy atom_type -percentual float]
+
+           Rod: -create -rod atom_type length width main_axis{X/Y/Z} [optional: -alloy atom_type -percentual float]
+
+           Tip (elliptic paraboloid): -create -tip atom_type z_max a b [optional: -alloy atom_type -percentual float]
+
+           Pyramid (square base): -create -pyramid atom_type z_max base_side_length [optional: -alloy atom_type -percentual float]
+
+           Cone: -create -cone atom_type z_max base_radius [optional: -alloy atom_type -percentual float]
+
+           Microscope: -create -microscope atom_type z_max_paraboloid a b z_max_pyramid base_side_length [optional: -alloy atom_type -percentual float]
+
+         ----------------
+         Merge Geometries
+         ----------------
+
+         -merge geom1.xyz geom2.xyz cutoff(Å)
+
+    '''
+    print(help_text)
+    sys.exit()
+# -------------------------------------------------------------------------------------
+def parse_translation(argv, inp):
+   """
+   Parse translation command line
 
    :argv : arguments list
    :inp  : input class
    """
    #
-   if argv[1] == '-h' or argv[1] == '-help':
-      print ('')
-      print ('  ==========')
-      print ('  Execution:')
-      print ('  ==========')
-      print ('')
-      print ('     * The inputs must contain each angle(degrees)/distance(Å) in different lines, without blank lines')
-      print ('')
-      print ('     ------------')
-      print ('     Translation:')
-      print ('     ------------')
-      print ('')
-      print ('     Distances in input (controlled dist. between 2 files):')
-      print ('')
-      print ('       -t distances_input geom1.xyz origin_CM_1{origin_CM_1_yes/no} geom2.xyz origin_CM_2{origin_CM_2_yes/no} axis{+-}{x/y/z} verbose{verbose_yes/no}')
-      print ('')
-      print ('     One translation:')
-      print ('')
-      print ('       -t1 shift geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}')
-      print ('')
-      print ('')
-      print ('     ---------')
-      print ('     Rotation:')
-      print ('     ---------')
-      print ('')
-      print ('     Angles in input:')
-      print ('')
-      print ('       -r angles_input geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}')
-      print ('')
-      print ('     One rotation:')
-      print ('')
-      print ('       -r1 angle geom.xyz origin_CM{origin_CM_yes/no} axis{+-}{x/y/z}')
-      print ('')
-      print ('')
-      print ('     -----------------')
-      print ('     Minimum Distance:')
-      print ('     -----------------')
-      print ('')
-      print ('     -min geom1.xyz geom2.xyz')
-      print ('')
-      print ('')
-      print ('     -------------------')
-      print ('     Geometrical Center:')
-      print ('     -------------------')
-      print ('')
-      print ('     -c geom.xyz')
-      print ('')
-      print ('')
-      print ('     ------------------')
-      print ('     Specular geometry:')
-      print ('     ------------------')
-      print ('')
-      print ('     -mirror geom.xyz')
-      print ('')
-      print ('')
-      print ('     -----------------')
-      print ('     Generate Geometry')
-      print ('     -----------------')
-      print ('')
-      print ('     Graphene:')
-      print ('')
-      print ('       Ribbon (X: zigzag | Y: armchair): -create -graphene rib X_length Y_length')
-      print ('')
-      print ('       Disk: -create -graphene disk radius')
-      print ('')
-      print ('       Ring: -create -graphene ring radius_out radius_in')
-      print ('')
-      print ('       Triangle: -create -graphene triangle edge_type{armchair/zigzag} side_length')
-      print ('')
-      print ('     Nanoparticles (Ag/Au/Na):')
-      print ('')
-      print ('       Sphere: -create -sphere atom_type radius center_x center_y center_z [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('       Rod: -create -rod atom_type length width main_axis{X/Y/Z} [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('       Tip (elliptic paraboloid): -create -tip atom_type z_max a b [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('       Pyramid (square base): -create -pyramid atom_type z_max base_side_length [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('       Cone: -create -cone atom_type z_max base_radius [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('       Microscope: -create -microscope atom_type z_max_paraboloid a b z_max_pyramid base_side_length [optional: -alloy atom_type -percentual float]')
-      print ('')
-      print ('     ----------------')
-      print ('     Merge Geometries')
-      print ('     ----------------')
-      print ('')
-      print ('     -merge geom1.xyz geom2.xyz cutoff(Å)')
-      print ('')
- 
-   
-      sys.exit()
-    
-   elif argv[1] == '-t':
+   if argv[1] == '-t':
       inp.translate_controlled_distance = True
 
       inp.distances_input = str(argv[2])
@@ -130,8 +158,16 @@ def read_command_line(argv,inp):
       inp.dir_axis_input = str(argv[5])
 
       if (inp.origin_CM == 'origin_CM_yes'): inp.move_geom_to_000 = True
-   
-   elif argv[1] == '-r':
+# -------------------------------------------------------------------------------------
+def parse_rotation(argv, inp):
+   """
+   Parse rotation command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   if argv[1] == '-r':
       inp.rotate_angles = True
 
       inp.angles_input   = str(argv[2])
@@ -150,129 +186,157 @@ def read_command_line(argv,inp):
       inp.dir_axis_input = str(argv[5])
 
       if (inp.origin_CM == 'origin_CM_yes'): inp.move_geom_to_000 = True
+# -------------------------------------------------------------------------------------
+def parse_mirror(argv, inp):
+   """
+   Parse mirror command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   inp.geom_specular = True
+   inp.geom_file = str(argv[2])
+# -------------------------------------------------------------------------------------
+def parse_merge(argv, inp):
+   """
+   Parse merge command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   inp.merge = True
+   inp.geom1_file = str(argv[2])
+   inp.geom2_file = str(argv[3])
+   inp.merge_cutoff = float(argv[4])
+# -------------------------------------------------------------------------------------
+def parse_min(argv, inp):
+   """
+   Parse min command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   inp.min_dist = True
+   inp.geom1_file = str(argv[2])
+   inp.geom2_file = str(argv[3])
+# -------------------------------------------------------------------------------------
+def parse_center(argv, inp):
+   """
+   Parse center command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   inp.geom_center = True
+   inp.geom_file = str(argv[2])
+# -------------------------------------------------------------------------------------
+def parse_create(argv, inp):
+   """
+   Parse create command line
+
+   :argv : arguments list
+   :inp  : input class
+   """
+   #
+   inp.create_geom = True 
+
+   # Determine the script's location
+   script_path = os.path.abspath(__file__)
+   # Get the directory containing the script
+   script_dir = os.path.dirname(script_path)
+   # Get upper directory
+   base_dir = os.path.dirname(script_dir)
    
-   elif argv[1] == '-min':
-      inp.min_dist = True
-      inp.geom1_file = str(argv[2])
-      inp.geom2_file = str(argv[3])
-   
-   elif argv[1] == '-c':
-      inp.geom_center = True
+   if (argv[2] == '-graphene'):
+      inp.gen_graphene = True
+      inp.atomtype = "c"
+      inp.graphene_structure = argv[3] 
 
-      inp.geom_file = str(argv[2])
+      if inp.graphene_structure not in inp.graphene_structures: 
+         output.error(f'Requested graphene structure "{inp.graphene_structure}" not recognised') 
 
-   elif argv[1] == '-mirror':
-      inp.geom_specular = True
+      elif inp.graphene_structure == "rib":
+         inp.X_length = float(argv[4])
+         inp.Y_length = float(argv[5])
 
-      inp.geom_file = str(argv[2])
-   
-   elif argv[1] == '-create':
+      elif inp.graphene_structure == 'disk':
+         inp.radius = float(argv[4])
 
-      inp.create_geom = True 
+      elif inp.graphene_structure == 'ring':
+         inp.radius_out = float(argv[4])
+         inp.radius_in  = float(argv[5])
+         if (inp.radius_in >= inp.radius_out): output.error(f'Inner radius must be smaller than Outter radius.')
 
-      # Determine the script's location
-      script_path = os.path.abspath(__file__)
-      # Get the directory containing the script
-      script_dir = os.path.dirname(script_path)
-      # Get upper directory
-      base_dir = os.path.dirname(script_dir)
-      
-      if (argv[2] == '-graphene'):
-         inp.gen_graphene = True
-         inp.atomtype = "c"
-         inp.graphene_structure = argv[3] 
+      elif inp.graphene_structure == 'triangle':
+         inp.graphene_edge_type = argv[4]
+         inp.side_length = float(argv[5])
+         if inp.graphene_edge_type not in inp.graphene_edge_types:
+            output.error(f'Requested edge type "{inp.graphene_edge_type}" not recognised')
 
-         if inp.graphene_structure not in inp.graphene_structures: 
-            output.error(f'Requested graphene structure "{inp.graphene_structure}" not recognised') 
-
-         elif inp.graphene_structure == "rib":
-            inp.X_length = float(argv[4])
-            inp.Y_length = float(argv[5])
-
-         elif inp.graphene_structure == 'disk':
-            inp.radius = float(argv[4])
-
-         elif inp.graphene_structure == 'ring':
-            inp.radius_out = float(argv[4])
-            inp.radius_in  = float(argv[5])
-            if (inp.radius_in >= inp.radius_out): output.error(f'Inner radius must be smaller than Outter radius.')
-
-         elif inp.graphene_structure == 'triangle':
-            inp.graphene_edge_type = argv[4]
-            inp.side_length = float(argv[5])
-            if inp.graphene_edge_type not in inp.graphene_edge_types:
-               output.error(f'Requested edge type "{inp.graphene_edge_type}" not recognised')
-
-         # Create bulk graphene dynamically
-         create_geom.create_ase_bulk_graphene(inp, base_dir)
-
-      else:
-         inp.atomtype = argv[3].lower()
-         if inp.atomtype not in inp.metal_atomtypes: output.error(f'Atom Type "{argv[3]}" not recognised')
-
-         if (argv[2] == '-sphere'): 
-            inp.gen_sphere = True
-            inp.radius = float(argv[4])
-            for i in range(3): inp.sphere_center[i] = float(argv[5+i])
-
-         elif (argv[2] == '-rod'): 
-            inp.gen_rod = True
-            inp.rod_length = float(argv[4])
-            inp.rod_width = float(argv[5])
-            inp.main_axis = argv[6].lower()
-            if inp.main_axis not in inp.axes: output.error(f"Axis {inp.main_axis} not recognized.") 
-
-         elif (argv[2] == '-tip'): 
-            inp.gen_tip = True
-            inp.z_max = float(argv[4])
-            inp.elliptic_parabola_a = float(argv[5])
-            inp.elliptic_parabola_b = float(argv[6])
-
-         elif (argv[2] == '-pyramid'): 
-            inp.gen_pyramid = True
-            inp.z_max = float(argv[4])
-            inp.side_length =  float(argv[5])
-
-         elif (argv[2] == '-microscope'): 
-            inp.gen_microscope = True
-            inp.z_max_paraboloid = float(argv[4])
-            inp.elliptic_parabola_a = float(argv[5])
-            inp.elliptic_parabola_b = float(argv[6])
-            inp.z_max_pyramid = float(argv[7])                                              
-            inp.side_length =  float(argv[8])
-
-         elif (argv[2] == '-cone'): 
-            inp.gen_cone = True
-            inp.z_max = float(argv[4])
-            inp.radius = float(argv[5])
-
-         # Create bulk metal dynamically
-         create_geom.create_ase_bulk_metal(inp, base_dir)
-
-         # Alloy case
-         if (argv[-4] == '-alloy'):
-            inp.alloy = True
-            inp.atomtype_alloy = argv[-3].lower()
-            if inp.atomtype_alloy not in inp.metal_atomtypes: output.error(f"Alloy atom type {inp.atomtype_alloy} not supported.")
-            if inp.atomtype_alloy == inp.atomtype: output.error(f"Alloy atom type coincides with original geometry atom type.")
-
-            inp.alloy_perc = float(argv[-1])
-            if (inp.alloy_perc == 0.0   or 
-                inp.alloy_perc == 100.0 or 
-                inp.alloy_perc  < 0.0   or
-                inp.alloy_perc  > 100.0): output.error(f'Alloy percentual requested {inp.alloy_perc} %. It must be greater than 0 and lower than 100')
-
-            inp.alloy_string = f'_alloy_{inp.atomtype_alloy}_{inp.alloy_perc:5.2f}_perc'
-
-
-   elif argv[1] == '-merge':
-      inp.merge = True
-      inp.geom1_file = str(argv[2])
-      inp.geom2_file = str(argv[3])
-      inp.merge_cutoff = float(argv[4])
+      # Create bulk graphene dynamically
+      create_geom.create_ase_bulk_graphene(inp, base_dir)
 
    else:
-      output.error('ERROR: Option not recognised. Try python3 geom -h')
+      inp.atomtype = argv[3].lower()
+      if inp.atomtype not in inp.metal_atomtypes: output.error(f'Atom Type "{argv[3]}" not recognised')
+
+      if (argv[2] == '-sphere'): 
+         inp.gen_sphere = True
+         inp.radius = float(argv[4])
+         for i in range(3): inp.sphere_center[i] = float(argv[5+i])
+
+      elif (argv[2] == '-rod'): 
+         inp.gen_rod = True
+         inp.rod_length = float(argv[4])
+         inp.rod_width = float(argv[5])
+         inp.main_axis = argv[6].lower()
+         if inp.main_axis not in inp.axes: output.error(f"Axis {inp.main_axis} not recognized.") 
+
+      elif (argv[2] == '-tip'): 
+         inp.gen_tip = True
+         inp.z_max = float(argv[4])
+         inp.elliptic_parabola_a = float(argv[5])
+         inp.elliptic_parabola_b = float(argv[6])
+
+      elif (argv[2] == '-pyramid'): 
+         inp.gen_pyramid = True
+         inp.z_max = float(argv[4])
+         inp.side_length =  float(argv[5])
+
+      elif (argv[2] == '-microscope'): 
+         inp.gen_microscope = True
+         inp.z_max_paraboloid = float(argv[4])
+         inp.elliptic_parabola_a = float(argv[5])
+         inp.elliptic_parabola_b = float(argv[6])
+         inp.z_max_pyramid = float(argv[7])                                              
+         inp.side_length =  float(argv[8])
+
+      elif (argv[2] == '-cone'): 
+         inp.gen_cone = True
+         inp.z_max = float(argv[4])
+         inp.radius = float(argv[5])
+
+      # Create bulk metal dynamically
+      create_geom.create_ase_bulk_metal(inp, base_dir)
+
+      # Alloy case
+      if (argv[-4] == '-alloy'):
+         inp.alloy = True
+         inp.atomtype_alloy = argv[-3].lower()
+         if inp.atomtype_alloy not in inp.metal_atomtypes: output.error(f"Alloy atom type {inp.atomtype_alloy} not supported.")
+         if inp.atomtype_alloy == inp.atomtype: output.error(f"Alloy atom type coincides with original geometry atom type.")
+
+         inp.alloy_perc = float(argv[-1])
+         if (inp.alloy_perc == 0.0   or 
+             inp.alloy_perc == 100.0 or 
+             inp.alloy_perc  < 0.0   or
+             inp.alloy_perc  > 100.0): output.error(f'Alloy percentual requested {inp.alloy_perc} %. It must be greater than 0 and lower than 100')
+
+         inp.alloy_string = f'_alloy_{inp.atomtype_alloy}_{inp.alloy_perc:5.2f}_perc'
 # -------------------------------------------------------------------------------------
 def check_file_exists(infile):
    #
