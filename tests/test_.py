1ccfa7d7c41dc65ed40545f242a0b57163a66fcd
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-02-20 15:02:09 +0100

    cto test added

--- tests/test_.py
+++ tests/test_.py
@@ -275,6 +275,34 @@ def test_create_icosahedra(monkeypatch):
    # Compare the generated file with the reference
    assert filecmp.cmp(generated_file, expected_file, shallow=False), "Generated XYZ file does not match the expected output"
 # -------------------------------------------------------------------------------------
+def test_create_cuboctahedra(monkeypatch):
+   """
+   Test if the generated cuboctahedral geometry matches the expected XYZ file.
+   """
+
+   # Test folder
+   test_folder = 'cuboctahedron'
+   
+   # Mock sys.argv to simulate the command line input
+   mock_args = ["dummy", '-create', '-cto', 'au', '50.0']
+   monkeypatch.setattr(sys, "argv", mock_args)
+   
+   # Manually create and populate the input class
+   inp = input_class.input_class()
+   general.read_command_line(sys.argv, inp)
+   
+   # Run the geometry creation
+   create_geom.select_case(inp)
+   
+   # Define the expected and actual output files
+   expected_file = os.path.join(os.path.dirname(__file__), test_folder, "reference", "cuboctahedron_au_r_50.0.xyz")
+   generated_file = f"{test_folder}/cuboctahedron_{inp.atomtype}_r_{inp.radius}{inp.alloy_string}.xyz"
+
+   move_created_geom(test_folder)
+   
+   # Compare the generated file with the reference
+   assert filecmp.cmp(generated_file, expected_file, shallow=False), "Generated XYZ file does not match the expected output"
+# -------------------------------------------------------------------------------------
 def test_create_microscope(monkeypatch):
    """
    Test if the generated microscope geometry matches the expected XYZ file.
