a718eb23d4939a7f959aac50e6cfd1937af72492
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-02-26 13:10:04 +0100

    install geom globally

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="geom",
+    version="1.0.0",
+    author="Your Name",
+    description="GEOM: A CLI for nanoplasmonics geometry manipulation",
+    packages=find_packages(),
+    install_requires=[
+        "ase==3.22.1",
+        "gmsh==4.11.1",
+        "numpy==1.24.3",
+        "pytest==8.3.4",  # Remove if not needed for installation
+    ],
+    entry_points={
+        "console_scripts": [
+            "geom = geom.__main__:main",  # Exposes 'geom' as a CLI command
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
