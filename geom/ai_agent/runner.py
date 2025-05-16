724f54e741811dbed24cfd005c0d53b72b951899
Pablo Grobas Illobre <pgrobasillobre@gmail.com>
2025-05-16 09:48:14 +0200

    solve runner problem + modify terminate after user request

--- geom/ai_agent/runner.py
+++ geom/ai_agent/runner.py
@@ -1,11 +1,11 @@
 import subprocess
 
 def run_geom_command(command: str):     
-    """Run the given GEOM CLI command using subprocess.
+   """Run the given GEOM CLI command using subprocess.
 
-    Args:
-        command (str): The GEOM command string to execute.
-    """
+   Args:
+       command (str): The GEOM command string to execute.
+   """
 
    subprocess.run(f"python3 -m geom {command}".split()) 
 
